3e8b7f032f2e9817d57b7806f369a871b8a777f6
livelace <o.popov@livelace.ru>
2021-08-04 21:54:55 +0300

    jte

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -12,6 +12,7 @@ libraries {
     mattermost
     shell {
         build = """
+            mkdir -p /conf /data
             echo \$BUILD_CONF | base64 -d > /conf/build.conf
             echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json
             echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore
