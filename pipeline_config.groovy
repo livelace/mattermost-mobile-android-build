393de8df1bb9f1a71e0a5a0cf9724b77db8240bf
livelace <o.popov@livelace.ru>
2021-08-04 21:09:02 +0300

    jte

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,6 +2,7 @@ libraries {
     git {
         repo_url = "https://github.com/livelace/mattermost-mobile-android-build.git"
     }
+    k8s_build
     kaniko {
         destination = "infra/mattermost-mobile-android-build:latest"
     }
