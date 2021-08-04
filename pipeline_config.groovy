28c3ef511d62ec6ae79f4549b21c60c7bd0210a8
livelace <o.popov@livelace.ru>
2021-08-04 21:57:05 +0300

    jte

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -13,9 +13,12 @@ libraries {
     shell {
         build = """
             mkdir -p /conf /data
+
             echo \$BUILD_CONF | base64 -d > /conf/build.conf
             echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json
             echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore
+
+            sed -i "s|<BRANCH>|release-1.31|g" /conf/build.conf
 
             /entrypoint.sh build
         """
