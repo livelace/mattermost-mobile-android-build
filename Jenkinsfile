8f82007d2e6add19a8ea3047043f80e4f1ef6fee
livelace <o.popov@livelace.ru>
2021-08-04 21:47:08 +0300

    jte

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,1 +1,7 @@
-k8s_build()
+def secret = [
+        ["BUILD_CONF", "secret/app/mattermost-mobile-android", "conf"],
+        ["BUILD_FIREBASE", "secret/app/mattermost-mobile-android", "firebase"]
+        ["BUILD_KEYSTORE", "secret/app/mattermost-mobile-android", "keystore"]
+]
+
+k8s_build(secret)
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,10 +2,21 @@ libraries {
     git {
         repo_url = "https://github.com/livelace/mattermost-mobile-android-build.git"
     }
-    k8s_build
-    kaniko {
-        context = "/tmp/job/work"
-        destination = "dev/mattermost-mobile-android-build:latest"
+    k8s_build {
+        image = "harbor-core.k8s-2.livelace.ru/dev/mattermost-mobile-android-build:latest"
     }
+//     kaniko {
+//         context = "/tmp/job/work"
+//         destination = "dev/mattermost-mobile-android-build:latest"
+//     }
     mattermost
+    shell {
+        build = """
+            echo \$BUILD_CONF | base64 -d > /conf/build.conf
+            echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json
+            echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore
+
+            /entrypoint.sh build
+        """
+    }
 }
