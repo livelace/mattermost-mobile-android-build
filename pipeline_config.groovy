3f9eb8e253a0890f1e17d4cb7acee0116bba30c7
livelace <o.popov@livelace.ru>
2021-08-05 16:22:50 +0300

    jte

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,35 +4,35 @@ libraries {
     git {
         repo_url = "https://github.com/livelace/mattermost-mobile-android-build.git"
     }
-//     harbor {
-//         policy = "mattermost-mobile-android-build"
-//     }
+    harbor {
+        policy = "mattermost-mobile-android-build"
+    }
     k8s_build {
         image = "harbor-core.k8s-2.livelace.ru/dev/mattermost-mobile-android-build:latest"
     }
-//     kaniko {
-//         context = "/tmp/job/work"
-//         destination = "dev/mattermost-mobile-android-build:latest"
-//     }
+    kaniko {
+        context = "/tmp/job/work"
+        destination = "dev/mattermost-mobile-android-build:latest"
+    }
     mattermost
-    nexus {
-        source = "/data/${env.BRANCH}/mattermost-mobile/matterlace.apk"
-        destination = "dists-internal/matterlace/matterlace-${env.BRANCH}.apk"
-        ignore_ssl = true
-    }
-    shell {
-        build = """
-            mkdir -p /conf /data && \
-
-            echo \$BUILD_CONF | base64 -d > /conf/build.conf && \
-            echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json && \
-            echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore && \
-
-            sed -i "s|<BRANCH>|${env.BRANCH}|g" /conf/build.conf && \
-
-            /entrypoint.sh build
-        """
-        timeout = TIMEOUT
-    }
+//     nexus {
+//         source = "/data/${env.BRANCH}/mattermost-mobile/matterlace.apk"
+//         destination = "dists-internal/matterlace/matterlace-${env.BRANCH}.apk"
+//         ignore_ssl = true
+//     }
+//     shell {
+//         build = """
+//             mkdir -p /conf /data && \
+//
+//             echo \$BUILD_CONF | base64 -d > /conf/build.conf && \
+//             echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json && \
+//             echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore && \
+//
+//             sed -i "s|<BRANCH>|${env.BRANCH}|g" /conf/build.conf && \
+//
+//             /entrypoint.sh build
+//         """
+//         timeout = TIMEOUT
+//     }
     utils
 }
