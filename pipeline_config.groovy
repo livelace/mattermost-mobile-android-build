0fd5913e363460d9fd0495a7a5909220b62c9891
Oleg 'livelace' Popov <o.popov@livelace.ru>
2024-05-28 00:40:13 +0300

    ci: refactor

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,42 +1,41 @@
 def TIMEOUT = env.TIMEOUT ? env.TIMEOUT.toInteger() : 120
 
 libraries {
-    git {
-        repo_url = "https://github.com/livelace/mattermost-mobile-android-build.git"
-    }
-    //harbor {
-    //    policy = "mattermost-mobile-android-build"
-    //}
-    k8s_build {
-        image = "registry.livelace.ru/dev/mattermost-mobile-android-build:latest"
-        force_pull = true
-
-        cpu_max = "16000m"
-        cpu_min = "8000m"
-
-        mem_max = "50Gi"
-        mem_min = "30Gi"
-
-        secret = """
+  git {
+    repo_url = "https://github.com/livelace/mattermost-mobile-android-build.git"
+  }
+  harbor {
+      policy = "mattermost-mobile-android-build"
+  }
+  k8s_build {
+    image = "registry.livelace.ru/dev/mattermost-mobile-android-build:latest"
+    force_pull = true
+    
+    cpu_max = "16000m"
+    cpu_min = "8000m"
+    
+    mem_max = "50Gi"
+    mem_min = "30Gi"
+    
+    secret = """
             env, BUILD_CONF, secret/app/mattermost-mobile-android, conf
             env, BUILD_FIREBASE, secret/app/mattermost-mobile-android, firebase
             env, BUILD_KEYSTORE, secret/app/mattermost-mobile-android, keystore
         """
-
-        timeout = TIMEOUT
-    }
-   kaniko {
-       context = "/tmp/job/work"
-       destination = "dev/mattermost-mobile-android-build:latest"
-   }
-    mattermost
-    nexus {
-        source = "/data/${env.BRANCH}/mattermost-mobile/matterlace.apk"
-        destination = "raw/matterlace/matterlace-${env.BRANCH}.apk"
-        ignore_ssl = true
-    }
-    shell {
-        build = """
+    
+    timeout = TIMEOUT
+  }
+  kaniko {
+    context = "/tmp/job/work"
+    destination = "dev/mattermost-mobile-android-build:latest"
+  }
+  mattermost
+  nexus {
+    source = "/data/${env.BRANCH}/mattermost-mobile/matterlace.apk"
+    destination = "raw/matterlace/matterlace-${env.BRANCH}.apk"
+  }
+  shell {
+    build = """
             echo \$BUILD_CONF | base64 -d > /conf/build.conf && \
             echo \$BUILD_FIREBASE | base64 -d > /conf/google-services.json && \
             echo \$BUILD_KEYSTORE | base64 -d > /conf/android-apk-signing.keystore && \
@@ -45,8 +44,8 @@ libraries {
 
             /entrypoint.sh build
         """
-
-        timeout = TIMEOUT
-    }
-    utils
+    
+    timeout = TIMEOUT
+  }
+  utils
 }
